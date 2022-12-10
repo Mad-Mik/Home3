8771c37f3dcd5d4c6564c593ac49998e2bb28a46
mikhail.madoyan <mikhail.madoyan@gmail.com>
2022-12-11 03:18:14 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte a = 12;
+            //byte b = 4;
+            //Console.WriteLine(a + b);
+            //Console.WriteLine(a - b);
+            //Console.WriteLine(a / b);
+            //Console.WriteLine(a * b);
+            //Console.WriteLine(Math.Sqrt(a));
+
+            //int x = 256;
+            //int z = 128;
+            //Console.WriteLine(x + z);
+            //Console.WriteLine(x - z);
+            //Console.WriteLine(x * z);
+            //Console.WriteLine(x / z);
+            //Console.WriteLine(Math.Sqrt(x));
+
+            //long q = 258963147;
+            //long w = -963258741;
+            //Console.WriteLine(q + w);
+            //Console.WriteLine(q - w);
+            //Console.WriteLine(q * w);
+            //Console.WriteLine(q / w);
+            //Console.WriteLine(Math.Sqrt(q));
+
+            //string s = "Hello ";
+            //string d = "World";
+            //Console.Write(s);
+            //Console.WriteLine(d);
+
+            //double o = 55.4;
+            //double p = 44.6;
+            //Console.WriteLine(o + p);
+            //Console.WriteLine(o - p);
+            //Console.WriteLine(o * p);
+            //Console.WriteLine(o / p);
+            //Console.WriteLine(Math.Sqrt(o));
+
+            //float k = 258.96378f;
+            //float l = 125.65478f;
+            //Console.WriteLine(k + l);
+            //Console.WriteLine(k - l);
+            //Console.WriteLine(k * l);
+            //Console.WriteLine(k / l);
+            //Console.WriteLine(Math.Sqrt(k));
+
+            //decimal m = 235.189m;
+            //decimal n = 223.458m;
+            //Console.WriteLine(m + n);
+            //Console.WriteLine(m - n);
+            //Console.WriteLine(m * n);
+            //Console.WriteLine(m / n);
+
+
+
+            //int[] num = { 4, 3, 1, 5, 2 };
+
+            //for (int i = 1; i < 5; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine();
+
+
+            //Array.Sort(num);
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(num[i] + "");
+            //}
+            //Console.WriteLine();
+
+
+            //Console.WriteLine(num.Max());
+            //Console.WriteLine(num.Min());
+
+
+            //1.1
+            //int a = 5;
+            //int b = 7;
+            //int c = 9;
+
+            //if (a < b && a < c)
+            //{
+            //    Console.WriteLine(true);
+            //}
+
+            //else if (b > a && b < c)
+            //{
+            //    Console.WriteLine(true);
+            //}
+
+            //else if (c > a && c > b)
+            //{
+            //    Console.WriteLine(true);
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine(false);
+            //}
+
+
+            //1.2
+            //int a = 11;
+            //int b = 9;
+            //int c = 15;
+            //int d = 5;
+
+            //if (a + b == c + d)
+            //{
+            //    Console.WriteLine(true);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(false);
+            //}
+
+
+            //1.3
+            //int[] numer = new int[] { 4, 2, 6, 8 };
+            //int res = 0;
+
+            //for (int i = 0; i < numer.Length; i++)
+            //{
+            //    if (numer[i] % 2 == 0)
+            //    {
+            //        res++;
+            //    }
+            //}
+
+            //if (res > 3) Console.WriteLine(1);
+            //else Console.WriteLine(2);
+        }
+    }
+}
